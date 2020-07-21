b3cfc591addc252d1881b0e9679bc0e26804a332
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-07-22 00:22:35 +0530

    Chapter1 :Getting your feet wet

---
+++ chapter_1.js
@@ -0,0 +1,41 @@
+
+//Excersise 1
+
+let joke = 'JavaScript walked into a bar....'; //A mistake here on Quotes is not consistant
+let toldJoke = false; ///This was written in string
+let $punchline = 'Better watch out for those semi-colons.';
+let entage = 20; ///There is an additional value in the variable entage
+let result
+if (toldJoke == true) {
+    //console.log($punchline);
+} else {
+    //console.log(joke);
+}
+
+//Excersise 2 (Variable and Values)
+
+let zipCode = 98104;
+let joesFavoriteMovie = "Forbidden Planet"; ///The variable name had a additinal character.
+let movieTicket$ = 9;
+if (movieTicket$ >= 9) {
+    //console.log("Too much!");
+} else {
+    //console.log("We're going to see " + joesFavoriteMovie);
+}
+
+//Excercise 3 (While loop)
+let scoops = 5;
+while(scoops > 1) {
+    //console.log('Another scoop');
+    scoops = scoops - 1;
+}
+//console.log('Life without ice cream isn\'t the same');
+
+
+let count = 1;
+while(count <=100) {
+    console.log(count);
+    count = count + 1;
+    //count = count -1;
+}
+console.log('This is end')
