9d252f071b26f615eed7dcbd94d85d11098b991c
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-07-23 00:18:45 +0530

    Chapter 2 in progress

---
+++ chapter_2.js
@@ -0,0 +1,15 @@
+let dogName = "rover";
+let dogWeight = 23;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
+
+dogName = "lady";
+dogWeight = 17;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
