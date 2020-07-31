f941809376a720deaff3cbd0073393e21ba72ea4
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-07-31 11:54:41 +0530

    Added the code

--- chapter_3.js
+++ chapter_3.js
@@ -1,2 +1,48 @@
 ////Javascript Functions;
 
+// var dogName = 'rover'
+// var dogweight = 23;
+// if (dogweight > 20) {
+//     console.log(dogName + ' says WOOF WOOF');
+// } else {
+//     console.log(dogName + ' says woof woof');
+// }
+
+// dogName = 'spot';
+// dogweight = 13;
+// if (dogweight > 20) {
+//     console.log(dogName + 'says WOOF WOOF')
+// } else {
+//     console.log(dogName + ' says woof woof');
+// }
+
+// dogName = "spike";
+// dogWeight = 53;
+// if (dogWeight > 20) {
+// console.log(dogName + " says WOOF WOOF");
+// } else {
+// console.log(dogName + " says woof woof");
+// }
+// dogName = "lady";
+// dogWeight = 17;
+// if (dogWeight > 20) {
+// console.log(dogName + " says WOOF WOOF");
+// } else {
+// console.log(dogName + " says woof woof");
+// }
+
+
+//////Function
+function bark(name , weight) {
+    if (weight > 20) {
+        console.log(name + ' says WOOF WOOF');
+    } else {
+        console.log(name + 'says woof woof');
+    }
+}
+
+bark('rover' , 23);
+bark('spot' , 13);
+bark('spike' , 53);
+bark('lady' , 17);
+
